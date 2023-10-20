6048243231d05bb1ccbe2b6c6c2fe44afe06ed14
Ethan Mikel <ethanmikel@Ethans-MacBook-Pro.local>
2023-10-20 00:05:48 -0500

    intial adding in  project, apply, and login pages

---
+++ Group6FinalProject/Group6FinalProject/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+//TODO: Make this namespace match your project name
+namespace Group_6_Final_Project.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+}

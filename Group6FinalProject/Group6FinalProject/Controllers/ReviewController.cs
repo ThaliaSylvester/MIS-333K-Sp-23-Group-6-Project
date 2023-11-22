be734410aa4aa383255621e60e76cc7f75f90c04
gam3727 <garrett.mixon@utexas.edu>
2023-11-22 13:24:48 -0500

    Review Manager

---
+++ Group6FinalProject/Group6FinalProject/Controllers/ReviewController.cs
@@ -0,0 +1,163 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Group_6_Final_Project.DAL;
+using Group_6_Final_Project.Models;
+
+namespace Group6FinalProject.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ReviewController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Review
+        public async Task<IActionResult> Index()
+        {
+              return _context.Reviews != null ? 
+                          View(await _context.Reviews.ToListAsync()) :
+                          Problem("Entity set 'AppDbContext.Reviews'  is null.");
+        }
+
+        // GET: Review/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Reviews == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Reviews
+                .FirstOrDefaultAsync(m => m.ReviewID == id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return View(review);
+        }
+
+        // GET: Review/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Review/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ReviewID,Rating,Description,Status")] Review review)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(review);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(review);
+        }
+
+        // GET: Review/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Reviews == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Reviews.FindAsync(id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+            return View(review);
+        }
+
+        // POST: Review/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ReviewID,Rating,Description,Status")] Review review)
+        {
+            if (id != review.ReviewID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(review);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReviewExists(review.ReviewID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(review);
+        }
+
+        // GET: Review/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Reviews == null)
+            {
+                return NotFound();
+            }
+
+            var review = await _context.Reviews
+                .FirstOrDefaultAsync(m => m.ReviewID == id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return View(review);
+        }
+
+        // POST: Review/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Reviews == null)
+            {
+                return Problem("Entity set 'AppDbContext.Reviews'  is null.");
+            }
+            var review = await _context.Reviews.FindAsync(id);
+            if (review != null)
+            {
+                _context.Reviews.Remove(review);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReviewExists(int id)
+        {
+          return (_context.Reviews?.Any(e => e.ReviewID == id)).GetValueOrDefault();
+        }
+    }
+}

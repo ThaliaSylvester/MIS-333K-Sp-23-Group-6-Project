fec04477be6786bd7add4a31b6d9d171e6e292ea
Ethan Mikel <54323448+ethanmikel@users.noreply.github.com>
2023-12-01 01:09:40 -0600

    Changes

--- Group6FinalProject/Group6FinalProject/Program.cs
+++ Group6FinalProject/Group6FinalProject/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddControllersWithViews();
 // Add a connection string here once you have created it on Azure
 //String connectionString = "Server=tcp:fa23group6finalproject.database.windows.net,1433;Initial Catalog=fa23Group6FinalProject;Persist Security Info=False;User ID=MISAdmin;Password=Password123@;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 String connectionString = "Server = tcp:fa23finalgroupproject.database.windows.net,1433; Initial Catalog = fa23groupfinalproject; Persist Security Info=False; User ID = MISAdmin; Password =Password123@; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;";
+String connectionString = "Server=tcp:fa23group6finalproject.database.windows.net,1433;Initial Catalog=fa23Group6FinalProject;Persist Security Info=False;User ID=MISAdmin;Password=Password123@;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+//String connectionString = "Server = tcp:fa23finalgroupproject.database.windows.net,1433; Initial Catalog = fa23groupfinalproject; Persist Security Info=False; User ID = MISAdmin; Password =Password123@; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;";
 
 //NOTE: This tells your application how to get a connection to the database
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
